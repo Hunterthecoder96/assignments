71f6686d3dccd918b29d6bbef48bda4857945497
John Skolnick <jhskolnick100@gmail.com>
2022-10-17 12:09:22 -0400

    API and Zen garden projects

--- capstonelvl2/app.js
+++ capstonelvl2/app.js
@@ -1,11 +1,31 @@
 // API given https://api.vschool.io/<yourname>/todo[/<todoId>]
 
 //Get
-axios.get("https://api.vschool.io/hunterSkolnick/todo")
-    .then(response=>console.log(response))
-    .catch(error=>console.log(error))
+// axios.get("https://api.vschool.io/hunterSkolnick/todo")
+//     .then(response=>console.log(response))
+//     .catch(error=>console.log(error))
 //post
-axios.post("https://api.vschool.io/hunterSkolnick/todo")
-//delete
 
+
+
+const todoForm = document.todoform
+
+todoForm.addEventListener("submit", function(event){  
+    event.preventDefault()
+
+const newTodo={
+    title: todoForm.title.value,
+    description: todoForm.description.value,
+       
+
+
+}
+axios.post("https://api.vschool.io/hunterSkolnick/todo", newTodo)
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+
+})
+
+//delete
+axios.delete("https://api.vschool.io/hunterSkolnick/todo", )
 //put
