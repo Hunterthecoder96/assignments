11cb7033a51228bbbbaa2a07bb509a03419226b8
John Skolnick <jhskolnick100@gmail.com>
2023-02-01 13:41:10 -0500

    organizing lessons

--- react-dice/src/App.js
+++ react-dice/src/App.js
@@ -1,36 +1,25 @@
-import React, {useState} from "react";
-import "./Die.css"
+import React, { useState } from 'react';
+import './Die.css';
+import Die from './Die';
 
+export default function App() {
+  const [number, setNumber] = useState([1, 2, 3, 4, 5]);
+  const random1 = Math.floor(Math.random() * 6) + 1;
+  const random2 = Math.floor(Math.random() * 6) + 1;
 
+  const roll = () => {
+    setNumber([random1, random2]);
+  };
 
-export default function App(){
-    const [number,setNumber] = useState([1,2,3,4,5])
-    const random1 = Math.floor(Math.random()*6)+1;
-    const random2 = Math.floor(Math.random()*6)+1;
-   
-
-    const roll =()=> {
-        setNumber(
-            [random1, random2]
-           
-        )
-    };
-
-    
-
-
-    return (
-        <div>
-            <div className="die">
-                
-                
-                <h1>{`${number[0]}`}</h1>
-                <h1>{` ${number[1]}`}</h1>
-            
-            </div>
-            <div>
-                <button onClick ={roll}>ROLL!</button>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="die">
+        <Die number={number[0]} className="dice1" />
+        <Die number={number[1]} className="dice2" />
+      </div>
+      <div>
+        <button onClick={roll}>ROLL!</button>
+      </div>
+    </div>
+  );
 }
--- react-dice/src/Die.js
+++ react-dice/src/Die.js
@@ -1,8 +1,17 @@
-import React from "react"
-import "./Die.css"
+import React from 'react';
+import './Die.css';
+import dice1 from './dicepics/dice1.PNG';
+import dice2 from './dicepics/dice2.PNG';
+import dice3 from './dicepics/dice3.PNG';
+import dice4 from './dicepics/dice4.PNG';
+import dice5 from './dicepics/dice5.PNG';
+import dice6 from './dicepics/dice6.PNG';
 
-export default function die(){
-    return(
-        <div className="square"></div>
-    )
+export default function die({ number }) {
+  const dicePics = [dice1, dice2, dice3, dice4, dice5, dice6];
+  return (
+    <div className="square">
+      <img src={dicePics[number - 1]} />
+    </div>
+  );
 }
